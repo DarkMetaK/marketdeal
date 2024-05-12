65555635552a5164d855919cc3c927b5c2c7fd96
albmarques <alberto.abrao@fatec.sp.gov.br>
2024-05-12 12:57:04 -0300

    start PreviousSignIn function

--- app/src/main/java/br/com/marketdeal/ui/activity/SignInActivity.kt
+++ app/src/main/java/br/com/marketdeal/ui/activity/SignInActivity.kt
@@ -1,6 +1,8 @@
 package br.com.marketdeal.ui.activity
 
+import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
@@ -12,16 +14,18 @@ import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 
 class SignInActivity : AppCompatActivity() {
-
     private val email by lazy { findViewById<EditText>(R.id.activity_sign_in_email) }
     private val password by lazy { findViewById<TextView>(R.id.activity_sign_in_password) }
     private val loginBtn by lazy { findViewById<Button>(R.id.activity_sign_in_login_btn) }
     private val signUpBtn by lazy { findViewById<TextView>(R.id.activity_sign_in_redirect_link) }
-
+    private lateinit var sp: SharedPreferences
     private val auth by lazy { Firebase.auth }
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        previousSignIn()
         super.onCreate(savedInstanceState)
+        sp = getSharedPreferences("MarketDeal_crud", MODE_PRIVATE)
+
         supportActionBar?.hide()
         setContentView(R.layout.activity_sign_in)
 
@@ -60,6 +64,14 @@ class SignInActivity : AppCompatActivity() {
         auth.signInWithEmailAndPassword(emailStr, passwordStr)
             .addOnCompleteListener(this) { task ->
                 if (task.isSuccessful) {
+                    sp.edit().apply{
+                        putString("email",emailStr)
+                        commit()
+                    }
+                    sp.edit().apply{
+                        putString("password",passwordStr)
+                        commit()
+                    }
                     val intent = Intent(this, MainActivity::class.java)
                     finish()
                     startActivity(intent)
@@ -69,6 +81,21 @@ class SignInActivity : AppCompatActivity() {
                         "Usuário não encontrado com este e-mail e senha.",
                         Toast.LENGTH_SHORT,
                     ).show()
+                }
+            }
+    }
+
+    private fun previousSignIn(){
+        val sp = this.getSharedPreferences("MarketDeal_crud", MODE_PRIVATE)
+        val emailStr = sp.getString("email","[]").toString()
+        val passwordStr = sp.getString("password","[]").toString()
+
+        auth.signInWithEmailAndPassword(emailStr, passwordStr)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    val intent = Intent(this, MainActivity::class.java)
+                    finish()
+                    startActivity(intent)
                 }
             }
     }
