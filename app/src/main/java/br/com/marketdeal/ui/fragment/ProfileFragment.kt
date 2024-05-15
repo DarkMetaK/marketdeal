639a6f10843fbce8cad8efe8538ab834f276e5d3
darkmetak <matporto03@gmail.com>
2024-05-15 10:02:21 -0300

    feat: Retrieving using data based on auth

--- app/src/main/java/br/com/marketdeal/ui/fragment/ProfileFragment.kt
+++ app/src/main/java/br/com/marketdeal/ui/fragment/ProfileFragment.kt
@@ -4,7 +4,7 @@ import android.os.Bundle
 import android.util.Log
 import android.widget.Button
 import android.widget.EditText
-import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import br.com.marketdeal.R
 import br.com.marketdeal.model.User
@@ -14,7 +14,6 @@ import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.ValueEventListener
 import com.google.firebase.database.getValue
 import com.google.firebase.database.ktx.database
-import com.google.firebase.database.values
 import com.google.firebase.ktx.Firebase
 
 class ProfileFragment : Fragment(R.layout.activity_profile) {
@@ -26,16 +25,16 @@ class ProfileFragment : Fragment(R.layout.activity_profile) {
     private val updateBtn by lazy { view?.findViewById<Button>(R.id.activity_profile_update_btn) }
     private val deleteBtn by lazy { view?.findViewById<Button>(R.id.activity_profile_delete_btn) }
 
-    private val database by lazy { Firebase.database.reference }
-    private val usersRef by lazy {
-        Firebase.database.getReference("users").child("d7aaC0tZXScv4lqyGWlCRTldlUO2")
-    }
+    private val userId by lazy { Firebase.auth.currentUser?.uid ?: null }
+    private val database by lazy { Firebase.database.getReference("users") }
+    private val userTableRef by lazy { userId?.let { database.child(it) } }
 
     private val userListener = object : ValueEventListener {
         override fun onDataChange(dataSnapshot: DataSnapshot) {
             val user = dataSnapshot.getValue<User>()
 
             if (user != null) {
+                Log.i("user", user.toString())
                 loadUserData(user)
             }
         }
@@ -48,7 +47,25 @@ class ProfileFragment : Fragment(R.layout.activity_profile) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        usersRef.addValueEventListener(userListener)
+        userTableRef?.addValueEventListener(userListener)
+        configUpdateBtn()
+    }
+
+    private fun configUpdateBtn() {
+        updateBtn?.setOnClickListener {
+            Log.i("Testando se clicou", "configUpdateBtn: |Teste")
+            val user = createUser()
+            updateUser(user)
+        }
+    }
+
+    private fun createUser(): User {
+        val nameStr = name?.text.toString()
+        val phoneStr = phone?.text.toString()
+        val emailStr = email?.text.toString()
+        val passwordStr = password?.text.toString()
+
+        return User(userId!!, emailStr, nameStr, phoneStr, passwordStr)
     }
 
     private fun loadUserData(user: User) {
@@ -58,4 +75,23 @@ class ProfileFragment : Fragment(R.layout.activity_profile) {
         password?.setText(user.password)
     }
 
+    private fun updateUser(user: User) {
+        val userMap = user.toMap()
+
+        database.updateChildren(userMap).addOnCompleteListener(requireActivity()) { task ->
+            if (task.isSuccessful) {
+                Toast.makeText(
+                    requireActivity(),
+                    "Perfil atualizado com sucesso!",
+                    Toast.LENGTH_SHORT,
+                ).show()
+            } else {
+                Toast.makeText(
+                    requireActivity(),
+                    "Falha ao atualizar os dados, por favor tente novamente.",
+                    Toast.LENGTH_SHORT,
+                ).show()
+            }
+        }
+    }
 }
