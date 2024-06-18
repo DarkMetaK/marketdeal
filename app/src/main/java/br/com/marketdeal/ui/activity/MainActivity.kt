77ff3b91f64e2ddf1460677dc671d9479055d1ce
Darkmetak <matporto03@gmail.com>
2024-06-18 13:26:07 -0300

    refactor: Remove nav items if user is not admin

--- app/src/main/java/br/com/marketdeal/ui/activity/MainActivity.kt
+++ app/src/main/java/br/com/marketdeal/ui/activity/MainActivity.kt
@@ -6,8 +6,12 @@ import androidx.navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
 import br.com.marketdeal.R
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
+    private val authUser by lazy { Firebase.auth.currentUser }
+
     private val navController by lazy { findNavController(R.id.activity_main_container) }
     private val navView by lazy { findViewById<BottomNavigationView>(R.id.activity_main_bn_navigation) }
 
@@ -22,6 +26,17 @@ class MainActivity : AppCompatActivity() {
 
     private fun setupBottomNavigation() {
         navView.setupWithNavController(navController)
+
+        if (authUser?.email != "admin@gmail.com") {
+            navView.removeItem(R.id.mi_store)
+            navView.removeItem(R.id.mi_product)
+        }
+    }
+
+    private fun BottomNavigationView.removeItem(id: Int) {
+        if (menu.findItem(id) != null) {
+            menu.removeItem(id)
+        }
     }
 
     private fun loadFragment() {
@@ -35,7 +50,6 @@ class MainActivity : AppCompatActivity() {
                 }
             }
         }
-
     }
 
 }
