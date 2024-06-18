0e3edebbc401f8a854894e64ac447a1ae0995a57
albmarques <alberto.abrao@fatec.sp.gov.br>
2024-06-18 18:41:36 -0300

    added image in market

--- app/src/main/java/br/com/marketdeal/ui/activity/MarketActivity.kt
+++ app/src/main/java/br/com/marketdeal/ui/activity/MarketActivity.kt
@@ -11,6 +11,7 @@ import br.com.marketdeal.R
 import br.com.marketdeal.model.Market
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
+import com.google.android.material.imageview.ShapeableImageView
 
 class MarketActivity : AppCompatActivity() {
     private val database by lazy { Firebase.database.reference }
@@ -19,33 +20,28 @@ class MarketActivity : AppCompatActivity() {
     private val number by lazy { findViewById<TextView>(R.id.activity_market_street_number) }
     private val neighborhood by lazy { findViewById<TextView>(R.id.activity_market_neighborhood) }
     private val city by lazy { findViewById<TextView>(R.id.activity_market_city) }
-    private  val deleteBtn by lazy { findViewById<Button>(R.id.activity_market_delete_btn) }
-    private  val editBtn by lazy { findViewById<Button>(R.id.activity_market_edit_btn) }
+    private val deleteBtn by lazy { findViewById<Button>(R.id.activity_market_delete_btn) }
+    private val editBtn by lazy { findViewById<Button>(R.id.activity_market_edit_btn) }
+    private val marketImage by lazy { findViewById<ShapeableImageView>(R.id.activity_market_image) }
     private lateinit var market: Market
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         supportActionBar?.hide()
         setContentView(R.layout.activity_market)
-
         retrieveIdAndFetchMarketData()
     }
 
     private fun retrieveIdAndFetchMarketData() {
         if (intent.hasExtra("id")) {
-            val id = intent.getSerializableExtra("id") as String
-
+            val id = intent.getStringExtra("id") as String
             database.child("markets").child(id).get().addOnSuccessListener {
                 val foundMarket = it.getValue(Market::class.java)
-
                 if (foundMarket != null) {
                     market = foundMarket
-
                     initializeFields()
                     configDeleteButton()
                     configEditButton()
-                    //retrieveMarketData()
                 }
             }.addOnFailureListener {
                 Log.e("firebase", "Error getting offer data", it)
@@ -54,18 +50,6 @@ class MarketActivity : AppCompatActivity() {
             finish()
         }
     }
-    /*
-        private fun retrieveMarketData() {
-            database.child("markets").child(market.id).get().addOnSuccessListener {
-                val market = it.getValue(Market::class.java)
-
-                if (market != null) {
-                    marketName.text = market.name
-                }
-            }.addOnFailureListener {
-                Log.e("firebase", "Error getting market data", it)
-            }
-        }*/
 
     private fun initializeFields() {
         name.text = market.name
@@ -82,21 +66,20 @@ class MarketActivity : AppCompatActivity() {
             "Estabelecimento deletado com sucesso!",
             Toast.LENGTH_SHORT,
         ).show()
-
         finish()
     }
 
     private fun configDeleteButton() {
-        deleteBtn.setOnClickListener{
+        deleteBtn.setOnClickListener {
             deleteMarket()
         }
     }
-    private fun configEditButton() {
-            editBtn.setOnClickListener {
-                val intent = Intent(this, MarketUpdateActivity::class.java)
-                intent.putExtra("id", market.uid)
-                startActivity(intent)
-            }
-    }
 
+    private fun configEditButton() {
+        editBtn.setOnClickListener {
+            val intent = Intent(this, MarketUpdateActivity::class.java)
+            intent.putExtra("id", market.uid)
+            startActivity(intent)
+        }
+    }
 }
